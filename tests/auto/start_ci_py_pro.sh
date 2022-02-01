397e560677469f319ce6ab9b5a188fd3919c6643
Venita Hagerty <venita.hagerty@noaa.gov>
2022-02-01 13:18:35 -0700

    Shell to start both ci_auto and ci_long

--- tests/auto/start_ci_py_pro.sh
+++ tests/auto/start_ci_py_pro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash --login
-set -eux
+set -eu
 
 #----------------------------------------------------------------------
 # This script loads the correct Python module for an HPC,
@@ -12,8 +12,6 @@ function usage {
   echo
   echo "       machine       [required] is one of: ${machines[@]}"
   echo "       py_prog       [required] Python program to run"
-  echo "       test_list     [optional] List of tests for ci_auto.py"
-  echo "                     default is auto_expts_list.txt - 2 tests"
   echo "       -h            display this help"
   echo
   return 1
@@ -29,20 +27,6 @@ export machine=$1
 machine=$(echo "${machine}" | tr '[A-Z]' '[a-z]')  # need lower case machine name
 
 export py_prog=$2
-
-if [[ $# -lt 3 ]] ; then
-   export test_list="auto_expts_list.txt"
-else
-   export test_list=$3
-fi
-
-if [[ ${py_prog} == "ci_auto.py" ]] ; then
-   if [ ! -f ${test_list} ]; then
-     echo "Test list file ${test_list} does not exist."
-     exit 1
-   fi
-   export py_prog="${py_prog} ${test_list}"
-fi
 
 if [[ ${machine} == hera ]]; then
   module use -a /contrib/miniconda3/modulefiles
